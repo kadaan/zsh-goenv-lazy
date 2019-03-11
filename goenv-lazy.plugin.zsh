1990d6827dda518633a877c42a66e900b6b2737c
Joel Baranick <joel.baranick@ensighten.com>
2019-03-11 16:22:47 -0700

    Fix completions location, now requires greadlink

--- goenv-lazy.plugin.zsh
+++ goenv-lazy.plugin.zsh
@@ -2,7 +2,7 @@
 if type goenv &> /dev/null; then
   local GOENV_SHIMS="${GOENV_ROOT:-${HOME}/.goenv}/shims"
   export PATH="${GOENV_SHIMS}:${PATH}"
-  source $(whence -p goenv)/../../completions/goenv.zsh
+  source $(dirname $(greadlink `whence -p goenv`))/../completions/goenv.zsh
   function goenv() {
     unset -f goenv > /dev/null 2>&1
     eval "$(command goenv init -)"
